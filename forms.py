18faae120d0e84b057d0625eed70df13ae745246
Jay Goel <dev@jaygoel.com>
2012-10-07 19:16:17 -0400

    fix password and whitespace validation

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask.ext.wtf import (Form, TextField, PasswordField, SubmitField,
                            Required, Email, Length, ValidationError)
+import string
 from models import User
 
 
@@ -14,7 +15,7 @@ class RegistrationForm(Form):
                       Length(max=128)])
     display_name = TextField("Display Name", validators=[Required(),
                              Length(max=32)])
-    password = PasswordField("Password", validators=[Required()])
+    password = PasswordField("Password", validators=[Required(), Length(min=8)])
     submit = SubmitField("Register")
 
     def validate_email(form, field):
@@ -22,10 +23,8 @@ class RegistrationForm(Form):
             raise ValidationError('Email address already taken')
 
     def validate_display_name(form, field):
-        # TODO: no whitespace
+        if [c for c in field.data if c in string.whitespace]:
+            raise ValidationError('Display name cannot contain whitespace')
+
         if User.query.filter_by(display_name=field.data).first():
             raise ValidationError('Display name already taken')
-
-    def validate_password(form, field):
-        # TODO: min password requirements
-        pass
