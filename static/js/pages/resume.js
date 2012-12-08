22b887c280c12f0e36e65c092d774b97464ee75d
Len Boyette <boyettel@gmail.com>
2012-12-07 19:39:01 -0500

    Converted load, add, and delete functions to angular.js

--- static/js/pages/resume.js
+++ static/js/pages/resume.js
@@ -1,95 +1,95 @@
 $(document).ready(function(){
     bootbox.animate(false);
     
-    $("button.addEducation").click(function(e) {
-        e.preventDefault();
-        addEducation();
-    });
+    // $("button.addEducation").click(function(e) {
+    //     e.preventDefault();
+    //     addEducation();
+    // });
 
-    $("button.addExperience").live('click',function(e) {
-        e.preventDefault();
+    // $("button.addExperience").live('click',function(e) {
+    //     e.preventDefault();
 
-        var expType = $(this).parent().parent().parent().find('.typeName').text();
-        addExperience(expType);
-    });
+    //     var expType = $(this).parent().parent().parent().find('.typeName').text();
+    //     addExperience(expType);
+    // });
 
-    $("button.addExpType").click(function(e) {
-        e.preventDefault();
+    // $("button.addExpType").click(function(e) {
+    //     e.preventDefault();
 
-        bootbox.prompt("Enter a new experience type name:", "Cancel", "OK", function(result) {
-            if (result) {
-                addExpType(result);
-            }        
-        });
-    });
+    //     bootbox.prompt("Enter a new experience type name:", "Cancel", "OK", function(result) {
+    //         if (result) {
+    //             addExpType(result);
+    //         }        
+    //     });
+    // });
 
     $("button.saveResume").click(function(e){
         e.preventDefault();
         saveResume(RESUMEIDNUM);
     });
 
-    $("a.removeEducation").live('click',function(e){
-        e.preventDefault();
+    // $("a.removeEducation").live('click',function(e){
+    //     e.preventDefault();
 
-        var ed = $(this).parent();
+    //     var ed = $(this).parent();
 
-        bootbox.dialog("Are you sure you want to remove this education?",
-            [
-            {
-                "label" : "Remove Education",
-                "class" : "btn-danger",
-                "callback": function() {
-                    ed.remove();
-                }
-            },
-            {
-                "label" : "Cancel",
-                "class" : "btn"
-            }
-            ]);
-    });
+    //     bootbox.dialog("Are you sure you want to remove this education?",
+    //         [
+    //         {
+    //             "label" : "Remove Education",
+    //             "class" : "btn-danger",
+    //             "callback": function() {
+    //                 ed.remove();
+    //             }
+    //         },
+    //         {
+    //             "label" : "Cancel",
+    //             "class" : "btn"
+    //         }
+    //         ]);
+    // });
 
-    $("a.removeExperience").live('click',function(e){
-        e.preventDefault();
+    // $("a.removeExperience").live('click',function(e){
+    //     e.preventDefault();
 
-        var ex = $(this).parent();
+    //     var ex = $(this).parent();
 
-        bootbox.dialog("Are you sure you want to remove this experience?",
-            [
-            {
-                "label" : "Remove Experience",
-                "class" : "btn-danger",
-                "callback": function() {
-                    ex.remove();
-                }
-            },
-            {
-                "label" : "Cancel",
-                "class" : "btn"
-            }
-            ]);
-    });
+    //     bootbox.dialog("Are you sure you want to remove this experience?",
+    //         [
+    //         {
+    //             "label" : "Remove Experience",
+    //             "class" : "btn-danger",
+    //             "callback": function() {
+    //                 ex.remove();
+    //             }
+    //         },
+    //         {
+    //             "label" : "Cancel",
+    //             "class" : "btn"
+    //         }
+    //         ]);
+    // });
 
-    $("a.removeExperienceType").live('click',function(e){
-        e.preventDefault();
+    // $("a.removeExperienceType").live('click',function(e){
+    //     e.preventDefault();
 
-        var exType = $(this).parent();
+    //     var exType = $(this).parent();
 
-        bootbox.dialog("Are you sure you want to remove this experience type?",
-            [
-            {
-                "label" : "Remove Experience Type",
-                "class" : "btn-danger",
-                "callback": function() {
-                    exType.remove();
-                }
-            },
-            {
-                "label" : "Cancel",
-                "class" : "btn"
-            }
-            ]);
-    });
+    //     bootbox.dialog("Are you sure you want to remove this experience type?",
+    //         [
+    //         {
+    //             "label" : "Remove Experience Type",
+    //             "class" : "btn-danger",
+    //             "callback": function() {
+    //                 exType.remove();
+    //             }
+    //         },
+    //         {
+    //             "label" : "Cancel",
+    //             "class" : "btn"
+    //         }
+    //         ]);
+    // });
 
     $("a.editExperienceType").live('click',function(e) {
         e.preventDefault();
@@ -118,94 +118,94 @@ function loadResume(resumeId){
     RESUMEIDNUM = resumeId;
     $.get("/resumes/" + String(resumeId) + "/resume.json", function(data){
         RESUME = (typeof data == "string") ? jQuery.parseJSON(data) : data;
-        loadObjective(RESUME);
-        loadContact(RESUME);
-        loadEducation(RESUME);
-        loadExperiences(RESUME);
+        //loadObjective(RESUME);
+        //loadContact(RESUME);
+        //loadEducation(RESUME);
+        //loadExperiences(RESUME);
     });
 }
 
-function loadObjective(resumeObject){
-    $("div.objective textarea").val(cleanUndefined(resumeObject.objective));
-}
+// function loadObjective(resumeObject){
+//     $("div.objective textarea").val(cleanUndefined(resumeObject.objective));
+// }
 
-function loadContact(resumeObject){
-    var nameToLoad = cleanUndefined(resumeObject.contact.name);
-    $("#contactForm div.name input").val(nameToLoad);
+// function loadContact(resumeObject){
+//     var nameToLoad = cleanUndefined(resumeObject.contact.name);
+//     $("#contactForm div.name input").val(nameToLoad);
 
-    var emailToLoad = cleanUndefined(resumeObject.contact.email);
-    $("#contactForm div.email input").val(emailToLoad);
+//     var emailToLoad = cleanUndefined(resumeObject.contact.email);
+//     $("#contactForm div.email input").val(emailToLoad);
 
-    var phoneToLoad = cleanUndefined(resumeObject.contact.phone);
-    $("#contactForm div.phone input").val(phoneToLoad);
+//     var phoneToLoad = cleanUndefined(resumeObject.contact.phone);
+//     $("#contactForm div.phone input").val(phoneToLoad);
 
-    var locationToLoad = cleanUndefined(resumeObject.contact.location);
-    $("#contactForm div.location input").val(locationToLoad);
-}
+//     var locationToLoad = cleanUndefined(resumeObject.contact.location);
+//     $("#contactForm div.location input").val(locationToLoad);
+// }
 
-function loadEducation(resumeObject){
-    $('#educationForms fieldset.educationForm').remove();
-    var educations = resumeObject.education;
-    for (var i = 0, ed; ed = educations[i]; i++){
-        addEducation(ed);
-    }
-}
+// function loadEducation(resumeObject){
+//     $('#educationForms fieldset.educationForm').remove();
+//     var educations = resumeObject.education;
+//     for (var i = 0, ed; ed = educations[i]; i++){
+//         addEducation(ed);
+//     }
+// }
 
-function loadExperiences(resumeObject){
-    $('#experienceTypes div.experienceType').remove();
+// function loadExperiences(resumeObject){
+//     $('#experienceTypes div.experienceType').remove();
 
-    var exs = resumeObject.experiences;
+//     var exs = resumeObject.experiences;
 
-    for (var exname in exs){
-        if(exs.hasOwnProperty(exname)){            
-            var ex = exs[exname];
-            for (var i = 0, e; e = ex[i];i++){
-                addExperience(exname,e);
-            }
-        }
-    }
-}
+//     for (var exname in exs){
+//         if(exs.hasOwnProperty(exname)){            
+//             var ex = exs[exname];
+//             for (var i = 0, e; e = ex[i];i++){
+//                 addExperience(exname,e);
+//             }
+//         }
+//     }
+// }
 
-function addEducation(educationObject){
-    var newEducationForm = educationTemplate.clone();
-    if(educationObject){        
-        newEducationForm.find('div.name input').val(cleanUndefined(educationObject.name));
-        newEducationForm.find('div.date input').val(cleanUndefined(educationObject.date));
-        newEducationForm.find('div.degree input').val(cleanUndefined(educationObject.degree));
-        newEducationForm.find('div.description textarea').val(cleanUndefined(educationObject.description));
-    }
-    $("#educationForms").append(newEducationForm);
-}
+// function addEducation(educationObject){
+//     var newEducationForm = educationTemplate.clone();
+//     if(educationObject){        
+//         newEducationForm.find('div.name input').val(cleanUndefined(educationObject.name));
+//         newEducationForm.find('div.date input').val(cleanUndefined(educationObject.date));
+//         newEducationForm.find('div.degree input').val(cleanUndefined(educationObject.degree));
+//         newEducationForm.find('div.description textarea').val(cleanUndefined(educationObject.description));
+//     }
+//     $("#educationForms").append(newEducationForm);
+// }
 
-function addExpType(experienceType){
-    if(experienceType){        
-        var newExpTypeForm = expTypeTemplate.clone();
-        newExpTypeForm.find('.experienceForm').remove();
-        newExpTypeForm.find('.typeName').text(experienceType);
-        $("#experienceTypes").append(newExpTypeForm);
-    }
-}
+// function addExpType(experienceType){
+//     if(experienceType){        
+//         var newExpTypeForm = expTypeTemplate.clone();
+//         newExpTypeForm.find('.experienceForm').remove();
+//         newExpTypeForm.find('.typeName').text(experienceType);
+//         $("#experienceTypes").append(newExpTypeForm);
+//     }
+// }
 
-function addExperience(experienceType, experienceObject){
-    var newExperienceForm = experienceTemplate.clone();
+// function addExperience(experienceType, experienceObject){
+//     var newExperienceForm = experienceTemplate.clone();
              
-    if(experienceObject){   
-        newExperienceForm.find('div.type select').val(cleanUndefined(experienceType));
-        newExperienceForm.find('div.name input').val(cleanUndefined(experienceObject.name));
-        newExperienceForm.find('div.location input').val(cleanUndefined(experienceObject.location));
-        newExperienceForm.find('div.title input').val(cleanUndefined(experienceObject.title));
-        newExperienceForm.find('div.date input').val(cleanUndefined(experienceObject.date));
-        newExperienceForm.find('div.description textarea').val(cleanUndefined(experienceObject.description));
-    }
-    var expTypeNode = $("#experienceInfo .typeName:contains('" + experienceType + "')");
+//     if(experienceObject){   
+//         newExperienceForm.find('div.type select').val(cleanUndefined(experienceType));
+//         newExperienceForm.find('div.name input').val(cleanUndefined(experienceObject.name));
+//         newExperienceForm.find('div.location input').val(cleanUndefined(experienceObject.location));
+//         newExperienceForm.find('div.title input').val(cleanUndefined(experienceObject.title));
+//         newExperienceForm.find('div.date input').val(cleanUndefined(experienceObject.date));
+//         newExperienceForm.find('div.description textarea').val(cleanUndefined(experienceObject.description));
+//     }
+//     var expTypeNode = $("#experienceInfo .typeName:contains('" + experienceType + "')");
 
-    if(!(expTypeNode.length)){
-        addExpType(experienceType);
-        expTypeNode = $("#experienceInfo .typeName:contains('" + experienceType + "')");
-    }
+//     if(!(expTypeNode.length)){
+//         addExpType(experienceType);
+//         expTypeNode = $("#experienceInfo .typeName:contains('" + experienceType + "')");
+//     }
 
-    expTypeNode.parent().find("#experienceForms").append(newExperienceForm);
-}
+//     expTypeNode.parent().find("#experienceForms").append(newExperienceForm);
+// }
 
 function saveResume(resumeId){
     saveObjective();
